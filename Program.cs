874a85e81712cfd1223bb03b872ed2d81ddeaf39
Mrbos <Mrbos@DESKTOP-JCFDELE>
2025-01-09 16:47:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DisplayMatrix
+{
+    using System;
+
+    public class MatrixDisplay
+    {
+        public static void Main(string[] args)
+        {
+            int[,] matrix = {
+            { 0, 1, 1, 1, 1 },
+            { 1, 0, 1, 1, 1 },
+            { 1, 1, 0, 1, 1 },
+            { 1, 1, 1, 0, 1 },
+            { 1, 1, 1, 1, 0 }
+        };
+
+            // Get the dimensions of the matrix
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            // Display the matrix
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine(); // Keep the console window open
+        }
+    }
+}
